17313a81a0a9021174ed5042f14a62651056b387
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-09-07 13:59:51 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,27 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
   return (
-    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
-      <div >
-        <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于Cloudflare Pages，开源于
-          <Link 
-          href="https://github.com/x-dr/telegraph-Image"
-          className="text-blue-300  hover:text-red-900 ml-1"
-          target="_blank"
-          rel="noopener noreferrer"
-          >GitHub Telegraph-Image</Link> </p>
-      </div>
-    </footer>
-  );
-}
+<footer className="w-full h-1/12 text-center bg-slate-200 flex flex-col justify-center items-center">
+  <div>
+    <p className="text-xs text-gray-500">Bản quyền Ⓒ 2024 Đã được bảo lưu. Xin đừng tải lên hình ảnh vi phạm pháp luật Việt Nam, người vi phạm sẽ phải chịu trách nhiệm. 
+    <br>
+      Chương trình này dựa trên <Link 
+      href="https://github.com/x-dr/telegraph-Image"
+      className="text-blue-300 hover:text-red-900 ml-1"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Telegram</Link> - <Link 
+      href="https://github.com/x-dr/telegraph-Image"
+      className="text-blue-300 hover:text-red-900 ml-1"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Cloudflare Pages</Link> -
+      <Link 
+      href="https://github.com/x-dr/telegraph-Image"
+      className="text-blue-300 hover:text-red-900 ml-1"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Telegraph-Image</Link> </p>
+  </div>
+</footer>
+
