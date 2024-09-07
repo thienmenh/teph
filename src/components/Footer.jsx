eaf122db466cdd93ff3c7aaa2c41beb2f8075de5
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-09-07 14:04:21 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,22 @@ export default function Footer() {
   return (
 <footer className="w-full h-1/12 text-center bg-slate-200 flex flex-col justify-center items-center">
   <div>
-    <p className="text-xs text-gray-500">Bản quyền Ⓒ 2024 Đã được bảo lưu. Xin đừng tải lên hình ảnh vi phạm pháp luật Việt Nam, người vi phạm sẽ phải chịu trách nhiệm. 
-    <br>
-      Chương trình này dựa trên <Link 
-      href="https://github.com/x-dr/telegraph-Image"
+    <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. Xin đừng tải lên hình ảnh vi phạm pháp luật Việt Nam, người vi phạm sẽ phải chịu trách nhiệm. 
+      <br>
+      Chương trình này dựa trên 
+      <Link 
+      href="https://pages.cloudflare.com/"
       className="text-blue-300 hover:text-red-900 ml-1"
       target="_blank"
       rel="noopener noreferrer"
-      >Telegram</Link> - <Link 
-      href="https://github.com/x-dr/telegraph-Image"
-      className="text-blue-300 hover:text-red-900 ml-1"
-      target="_blank"
-      rel="noopener noreferrer"
-      >Cloudflare Pages</Link> -
+      >Cloudflare Pages</Link> - 
       <Link 
       href="https://github.com/x-dr/telegraph-Image"
       className="text-blue-300 hover:text-red-900 ml-1"
       target="_blank"
       rel="noopener noreferrer"
-      >Telegraph-Image</Link> </p>
+      >Telegraph-Image</Link> 
+    </p>
   </div>
 </footer>
 
