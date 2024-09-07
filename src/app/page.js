bf881d156250ec7f5e5555f0c654e2179b27cc4a
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-09-07 15:03:57 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -329,7 +329,7 @@ export default function Home() {
   return (
     <main className=" overflow-auto h-full flex w-full min-h-screen flex-col items-center justify-between">
       <header className="fixed top-0 h-[50px] left-0 w-full border-b bg-white flex z-50 justify-center items-center">
- <nav className="flex justify-between items-center w-full max-w-4xl px-4"><a href="/"> IMG.BIBICA.NET</a></nav>
+ <nav className="flex justify-between items-center w-full max-w-4xl px-4"><a href="/"> HOME</a></nav>
 
       </header>
       <div className="mt-[60px] w-9/10 sm:w-9/10 md:w-9/10 lg:w-9/10 xl:w-3/5 2xl:w-2/3">
