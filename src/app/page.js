db0c9104b3bb94a575b09bdaee45132af13f8af5
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-09-07 15:10:02 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -337,7 +337,8 @@ export default function Home() {
           <div className="flex flex-col">
             <div className="text-gray-800 text-lg">Ảnh hoặc video tải lên</div>
             <div className="mb-4 text-sm text-gray-500">
-              Kích thước tệp tải lên tối đa 100 MB; Đã lưu trữ tổng cộng <span className="text-cyan-600">{Total}</span> bức ảnh
+              Kích thước tệp tải lên tối đa <span className="text-cyan-600">100</span> MB - Đã lưu trữ tổng cộng <span className="text-cyan-600">{Total}</span> bức ảnh <br>
+              Các định dạng hỗ trợ <span className="text-cyan-600">image/*,video/*</span>
             </div>
           </div>
 
