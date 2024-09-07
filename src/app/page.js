b0609f7bfbe407ec53649e9f06b094406ecaa973
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-09-08 02:58:23 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -333,6 +333,7 @@ export default function Home() {
     <a href="/" className="text-lg font-semibold text-gray-800">Trang Chủ</a>
   </nav>
 </header>
+     
 <div className="flex flex-col mb-4">
   <h1 className="text-2xl font-bold text-gray-800">Tải lên Ảnh hoặc Video</h1>
   <p className="text-sm text-gray-600">
